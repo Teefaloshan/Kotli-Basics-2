5e420e8ef8c8c441125b30a22186ce4730183286
Teefaloshan <105608503+Teefaloshan@users.noreply.github.com>
2023-01-01 21:31:58 +0300

    Add files via upload

---
+++ Exercise1.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication1
+
+class Exercise1 {
+    fun main(){
+        var a: String = "Abdullah"
+        var b: Float = 20.6F
+        var c: Double = 8.9746325
+        var d: Byte = 25
+        var e: Short = 2022
+        var f: Long = 321984526954
+        var g: Boolean = true
+        var h: Char = 'a'
+
+        println(a)
+        println(b)
+        println(c)
+        println(d)
+        println(e)
+        println(f)
+        println(g)
+        println(h)
+    }
+}
